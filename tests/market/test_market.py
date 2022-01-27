05cd7d61d2fce2566ac84d371d46a10e0864bf68
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-01-27 13:54:21 +0100

    test: add tests for different price scenarios

--- tests/market/test_market.py
+++ tests/market/test_market.py
@@ -14,3 +14,33 @@ def test_exact_match():
     assert market.orders == []
     assert market.accounts[0] == Account(cash_balance=0.5, stock_balance=2)
     assert market.accounts[1] == Account(cash_balance=2.5, stock_balance=1)
+
+
+def test_price_mismatch():
+    market = Market(
+        accounts=[
+            Account(cash_balance=5, stock_balance=5),
+            Account(cash_balance=6, stock_balance=6),
+        ],
+        orders=[],
+    )
+    market.submit_(BuyOrder(market.accounts[0], price=2, volume=1))
+    market.submit_(SellOrder(market.accounts[1], price=3, volume=1))
+    assert len(market.orders) == 2
+    assert market.accounts[0] == Account(cash_balance=5, stock_balance=5)
+    assert market.accounts[1] == Account(cash_balance=6, stock_balance=6)
+
+
+def test_crossed_price():
+    market = Market(
+        accounts=[
+            Account(cash_balance=5, stock_balance=5),
+            Account(cash_balance=6, stock_balance=6),
+        ],
+        orders=[],
+    )
+    market.submit_(BuyOrder(market.accounts[0], price=3, volume=1))
+    market.submit_(SellOrder(market.accounts[1], price=2, volume=1))
+    assert market.orders == []
+    assert market.accounts[0] == Account(cash_balance=3, stock_balance=6)
+    assert market.accounts[1] == Account(cash_balance=8, stock_balance=5)
