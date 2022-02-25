932c576a2d5dacb2a41a033e75532ebd006a1fc4
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-02-25 16:34:17 +0100

    explor: mocking replay buffer

--- tradezoo/trainer/trainer.py
+++ tradezoo/trainer/trainer.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
 import torch
-from typing import Dict
+from typing import Dict, List
 
 from tradezoo.agent import Agent, ObservationBatch
 from tradezoo.game import Game, TurnResult
+from .experience import Experience
 from .replay_buffer import ReplayBuffer
 
 
@@ -11,24 +12,32 @@ from .replay_buffer import ReplayBuffer
 class Trainer:
     game: Game
     replay_buffers: Dict[Agent, ReplayBuffer]
+    batch_size: int
 
     @classmethod
-    def new(cls, game: Game, replay_buffer_capacity: int) -> "Trainer":
+    def new(cls, game: Game, replay_buffer_capacity: int, batch_size: int) -> "Trainer":
         return cls(
             game=game,
             replay_buffers={
                 trader.agent: ReplayBuffer.empty(capacity=replay_buffer_capacity)
                 for trader in game.traders
             },
+            batch_size=batch_size,
         )
 
     def turn_(self) -> TurnResult:
         turn_result = self.game.turn_()
-        self.replay_buffers[turn_result.trader.agent].register_turn_(turn_result)
+        replay_buffer = self.replay_buffers[turn_result.trader.agent]
+        replay_buffer.register_turn_(turn_result)
+        if len(replay_buffer.experiences) >= self.batch_size:
+            self.train_(
+                agent=turn_result.trader.agent,
+                experiences=replay_buffer.sample(self.batch_size),
+            )
         return turn_result
 
-    def train_step_(self, agent: Agent, batch_size: int):
-        experiences = self.replay_buffers[agent].sample(batch_size)
+    @classmethod
+    def train_(cls, agent: Agent, experiences: List[Experience]):
         old_observations = ObservationBatch(
             observations=[experience.old_observation for experience in experiences]
         )
