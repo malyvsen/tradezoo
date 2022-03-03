121ae3f6d0716745ce22218642bc9439ba1e11e3
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-03-03 16:29:22 +0100

    explore: input normalization

--- tradezoo/agent/actor.py
+++ tradezoo/agent/actor.py
@@ -6,7 +6,11 @@ from .log_squish import LogSquish
 class Actor(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.distribution_parameters = torch.nn.Parameter(torch.randn([4]))
+        self.network = torch.nn.Sequential(
+            torch.nn.Linear(4, 16),
+            LogSquish(),
+            torch.nn.Linear(16, 4),
+        )
 
     def forward(self, observations: torch.Tensor) -> torch.Tensor:
-        return torch.tile(self.distribution_parameters, [observations.shape[0], 1])
+        return self.network(observations)
