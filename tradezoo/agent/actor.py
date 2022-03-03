496aedc5cbcba6df579d92ab67731892fc1a38d9
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-03-03 16:12:50 +0100

    explore: input-less actor

--- tradezoo/agent/actor.py
+++ tradezoo/agent/actor.py
@@ -6,11 +6,7 @@ from .log_squish import LogSquish
 class Actor(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.network = torch.nn.Sequential(
-            torch.nn.Linear(4, 16),
-            LogSquish(),
-            torch.nn.Linear(16, 4),
-        )
+        self.distribution_parameters = torch.nn.Parameter(torch.randn([4]))
 
     def forward(self, observations: torch.Tensor) -> torch.Tensor:
-        return self.network(observations)
+        return torch.tile(self.distribution_parameters, [observations.shape[0], 1])
