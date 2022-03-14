070af3f162ee8af95fedee97e139212a48757e6a
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-03-14 10:37:25 +0100

    explore: reward with mock critic

--- tradezoo/plots/__init__.py
+++ tradezoo/plots/__init__.py
@@ -1,6 +1,6 @@
 from .balance_plot import balance_plot
 from .decision_plot import decision_plot
-from .td_error_plot import td_error_plot
+from .reward_plot import reward_plot
 from .trades_plot import trades_plot
 from .training_plot import training_plot
 from .uncertainty_plot import uncertainty_plot
--- tradezoo/plots/reward_plot.py
+++ tradezoo/plots/reward_plot.py
@@ -4,13 +4,27 @@ from typing import List
 from tradezoo.game import TurnResult
 
 
-def td_error_plot(turn_results: List[TurnResult]):
+def reward_plot(turn_results: List[TurnResult]):
     return go.Figure(
         layout=dict(
             xaxis_title="Turn number",
-            yaxis_title="TD error",
+            yaxis_title="Reward",
         ),
         data=[
+            go.Scatter(
+                name="Instantaneous reward",
+                y=[turn_result.reward for turn_result in turn_results[:-1]],
+            ),
+            go.Scatter(
+                name="Expected future reward",
+                y=[
+                    turn_result.trader.agent.evaluate(
+                        turn_result.observation.batch
+                    ).item()
+                    * (1 - turn_result.trader.agent.discount_factor)
+                    for turn_result in turn_results[1:]
+                ],
+            ),
             go.Scatter(
                 name="TD error",
                 y=[
