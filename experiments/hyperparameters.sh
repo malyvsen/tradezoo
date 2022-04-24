3f74af8853f39f293e5262ea18fc6f38cdb71029
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-04-24 19:06:41 +0200

    chore: adjust alvis python module

--- experiments/hyperparameters.sh
+++ experiments/hyperparameters.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # This script starts a batch job on Alvis (https://www.c3se.chalmers.se/about/Alvis/)
-module load GCCcore/11.2.0 Python/3.9.6
+module load Python/3.9.6-GCCcore-11.2.0
 poetry run python ./hyperparameters.py
